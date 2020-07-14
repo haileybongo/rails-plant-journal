6aa9a3cf1db6e08c969f0c7e005628b720744ca7
haileybongo <haileyebongo@gmail.com>
2020-07-14 15:16:51 -0700

    adding footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,43 +10,13 @@ class User < ApplicationRecord
 
         needs_water = []
         self.journals.each do |journal|
-          if journal.water && journal.water.weeks && journal.last_watered
+          if journal.water.weeks && journal.last_watered
             days_since_watered = Date.today - journal.last_watered
-            case journal.water.weeks 
-
-            when 1
-                if days_since_watered > 7 
-                    needs_water << journal
-                else
-                end
-            when 2
-                if days_since_watered > 14 
-                    needs_water << journal
-                else
-                end
-            when 3
-                if days_since_watered > 21 
-                    needs_water << journal
-                else
-                end
-            when 4
-                if days_since_watered > 28 
-                    needs_water << journal
-                else
-                end
-            when 5
-                if days_since_watered > 36 
-                    needs_water << journal
-                else
-                end
-            when 6
-                if days_since_watered > 43 
-                    needs_water << journal
-                else
-                end
+            if journal.water.weeks * 7 < days_since_watered
+                needs_water << journal
             else
             end
-        end
+            end
         end
         needs_water
     end
